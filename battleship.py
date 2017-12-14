8ad128aedd9a04da20615d3b1cc8d1c4ad3a6e8b
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-12-14 14:51:04 -0500

    Updated from Brython Server: 12/14/2017 2:51:02 PM

--- battleship.py
+++ battleship.py
@@ -1,1 +1,33 @@
+#Charlie Goodrich
+#12/14/17
+#battleship.py - creates the game of battleship (please don't fail me)
 
+from ggame import *
+from random import randint
+
+#CONSTANTS
+EMPTY = 0
+MISS = 1
+HIT = 2
+
+def buildBoard():
+    data["board"] = [[EMPTY]*5,[EMPTY]*5,[EMPTY]*5,[EMPTY]*5,[EMPTY]*5]
+    for row in range(0,5):
+        for column in range(0,5):
+            print(data["board"][row][column], end = " ")
+        print()
+    
+
+if __name__ == "__main__":
+    
+    data = {}
+    board = [["a", "b", "c"],["d", "e", "f"],["g", "h", "i"]]
+
+    buildBoard()
+
+    
+    white = Color(0xFFFFFF, 1)
+    red = Color(0xFF0000, 1)
+    black = Color(0x000000, 1)
+    
+    
